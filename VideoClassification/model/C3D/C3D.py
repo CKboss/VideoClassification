7a79467b055238a08e3dfd291fafd491f6d87e5b
CKboss <qhn999@2008.sina.com>
2017-07-28 17:29:24 +0800

    extract_optial_flow

---
+++ VideoClassification/model/C3D/C3D.py
@@ -0,0 +1,16 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+
+class C3D(nn.Module):
+
+    def __init__(self):
+        super(nn.Module,self).__init__()
+
+    def forward(self,x):
+        pass
+
+
+if __name__=='__main__':
+    pass
---
+++ VideoClassification/model/densenet_twostream/densenet_twostream.py
@@ -0,0 +1,8 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+
+
+if __name__=='__main__':
+    pass
