88c2affe5818bf1feb1945e26a14ac29178fbfa3
CKboss <qhn999@2008.sina.com>
2017-07-28 13:24:11 +0800

    add tensorboard Logger

---
+++ VideoClassification/test/tensorboardertest.py
@@ -0,0 +1,14 @@
+from VideoClassification.utils.Logger import Logger
+
+import math
+
+log = Logger("/tmp/runs/r3")
+
+for step in range(10):
+    v1 = math.sin(step)
+    v2 = math.cos(step)
+    v3 = 1000*step
+    log.scalar_summary('v1',v1,step)
+    log.scalar_summary('v2',v2,step)
+    log.scalar_summary('v3',v3,step)
+
