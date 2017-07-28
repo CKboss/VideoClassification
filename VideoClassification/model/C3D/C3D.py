d920b178efb5d82225aa3a17ae449b4ef61cbb0e
CKboss <qhn999@2008.sina.com>
2017-07-28 20:18:35 +0800

    C3D

--- VideoClassification/model/C3D/C3D.py
+++ VideoClassification/model/C3D/C3D.py
@@ -6,11 +6,57 @@ from torch.autograd import Variable
 class C3D(nn.Module):
 
     def __init__(self):
-        super(nn.Module,self).__init__()
+
+        super(C3D,self).__init__()
+
+        self.conv1 = nn.Conv3d(3,64,kernel_size=3,padding=1)
+        self.mxpool1 = nn.MaxPool3d((1,2,2))
+        self.conv2 = nn.Conv3d(64,128,kernel_size=3,padding=1)
+        self.mxpool2 = nn.MaxPool3d(2)
+        self.conv3 = nn.Conv3d(128,256,kernel_size=3,padding=1)
+        self.conv4 = nn.Conv3d(256,256,kernel_size=3,padding=1)
+        self.mxpool3 = nn.MaxPool3d(2)
+        self.conv5 = nn.Conv3d(256,512,kernel_size=3,padding=1)
+        self.conv6 = nn.Conv3d(512,512,kernel_size=3,padding=1)
+        self.mxpool4 = nn.MaxPool3d(2)
+        self.conv7 = nn.Conv3d(512,512,kernel_size=3,padding=1)
+        self.conv8 = nn.Conv3d(512,512,kernel_size=3,padding=1)
+        self.mxpool5 = nn.MaxPool3d(2)
+        self.fc1 = nn.Linear(512*9,4096)
+        self.fc2 = nn.Linear(4096,101)
+
 
     def forward(self,x):
-        pass
+
+        x = self.conv1(x)
+        x = self.mxpool1(x)
+        x = self.conv2(x)
+        x = self.mxpool2(x)
+        x = self.conv3(x)
+        x = self.conv4(x)
+        x = self.mxpool3(x)
+        x = self.conv5(x)
+        x = self.conv6(x)
+        x = self.mxpool4(x)
+        x = self.conv7(x)
+        x = self.conv8(x)
+        x = self.mxpool5(x)
+
+        x = x.view(-1,512*9)
+
+        x = self.fc1(x)
+        self.midfeatures = x
+        x = self.fc2(x)
+
+        return x
 
 
 if __name__=='__main__':
-    pass
+
+    module = C3D().cuda()
+
+    x = torch.randn(2,3,16,112,112)
+    x = Variable(x).cuda()
+
+    y = module(x)
+    z = module.midfeatures
--- VideoClassification/model/densenet_twostream/densenet_twostream.py
+++ VideoClassification/model/densenet_twostream/densenet_twostream.py
@@ -3,6 +3,70 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.autograd import Variable
 
+import VideoClassification.Config.Config as Config
+from VideoClassification.model.densenet_twostream.densenet import densenet161,densenet201
+
+class dense_twostram(nn.Module):
+
+    def __init__(self,pretrained=False,in_channels=3,num_classes=101,level=161,drop1=0.9,drop2=0.8):
+
+        super(dense_twostram,self).__init__()
+
+        if level==161:
+            DENSEnet = densenet161
+        elif level==201:
+            DENSEnet = densenet201
+        else:
+            raise NotImplementedError('level should be 161 or 201')
+
+        self.dense = DENSEnet(in_channels=in_channels)
+
+        self.train_classification = nn.Sequential(
+            nn.Linear(2208,1024),
+            nn.ReLU(True),
+            nn.Dropout(drop1),
+            nn.Linear(1024,num_classes),
+        )
+
+        self.test_classfication = nn.Sequential (
+            nn.Linear(2208,1024),
+            nn.Linear(1024,num_classes),
+        )
+
+    def forward(self,x):
+        x = self.dense(x)
+        x = self.train_classification(x)
+        return x
+
+    def inference(self,x):
+        x = self.dense(x)
+        x = self.test_classfication(x)
+        return x
+
+def dense_spatialNet(pretrained=False,**kwargs):
+    return dense_twostram(pretrained=pretrained,in_channels=3,drop1=0.8,**kwargs)
+
+def dense_temporalNet(pretrained=False,**kwargs):
+    return dense_twostram(pretrained=pretrained,in_channels=20,drop1=0.9,**kwargs)
+
+def dense161_spatialNet(pretrained=False):
+    return dense_spatialNet(pretrained=pretrained,level=161)
+
+def dense161_temporalNet(pretrained=False):
+    return dense_temporalNet(pretrained=pretrained,level=161)
+
+def dense201_spatialNet(pretrained=False):
+    return dense_spatialNet(pretrained=pretrained,level=201)
+
+def dense201_temporalNet(pretrained=False):
+    return dense_temporalNet(pretrained=pretrained,level=201)
 
 if __name__=='__main__':
-    pass
+
+    x = torch.randn(2,3,224,224)
+    x = Variable(x).cuda()
+
+    module = dense161_spatialNet().cuda()
+
+    y = module(x)
+    print(y.size())
