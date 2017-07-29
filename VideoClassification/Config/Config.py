7ce1a1c20aed37e248b56a1ffd899d26cdeeb655
CKboss <qhn999@2008.sina.com>
2017-07-29 21:26:44 +0800

    add ServerConfig

--- VideoClassification/Config/Config.py
+++ VideoClassification/Config/Config.py
@@ -15,3 +15,11 @@ UCF101_images_root = './'
 
 ''' server '''
 
+'''
+
+vgg16pretrainfile = '/datacenter/1/UCF_Data/pretrainedmodel/vgg16-397923af.pth'
+ExWorkSpace = '/datacenter/1/UCF_Data/EX/'
+Code_root = '/datacenter/1/UCF_Data/EX/Code/VideoClassification/'
+UCF101_images_root = '/datacenter/1/UCF_Data/UCF101_images/'
+
+'''
