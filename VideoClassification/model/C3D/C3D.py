0267ca2c0d0e69408af471f9e0f0afc14d3b94b1
CKboss <qhn999@2008.sina.com>
2017-08-01 10:33:24 +0800

    ....

--- VideoClassification/model/C3D/C3D.py
+++ VideoClassification/model/C3D/C3D.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+from torch.nn import init
 import torch.nn.functional as F
 from torch.autograd import Variable
 
@@ -86,19 +87,16 @@ class C3D(nn.Module):
 
 
     def _initialize_weights(self):
-        import math
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
-                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
-                m.weight.data.normal_(0, math.sqrt(2. / n))
+                init.xavier_normal(m.weight.data)
                 if m.bias is not None:
                     m.bias.data.zero_()
             elif isinstance(m, nn.BatchNorm2d):
                 m.weight.data.fill_(1)
                 m.bias.data.zero_()
             elif isinstance(m, nn.Linear):
-                n = m.weight.size(1)
-                m.weight.data.normal_(0, 0.01)
+                init.xavier_normal(m.weight.data)
                 m.bias.data.zero_()
 
 
