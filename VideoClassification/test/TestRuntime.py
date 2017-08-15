591bb49c1fcb98f77ba857e8906131a842af3a3b
CKboss <qhn999@2008.sina.com>
2017-08-15 21:09:40 +0800

    ....

--- VideoClassification/test/TestRuntime.py
+++ VideoClassification/test/TestRuntime.py
@@ -9,7 +9,9 @@ except:
     import cv2
 
 import VideoClassification.Config.Config as Config
-from VideoClassification.model.resnet_twostream.resnet_twostream import resnet152_SpatialNet,resnet101_SpatialNet
+from VideoClassification.model.resnet_twostream.resnet_twostream import resnet152_SpatialNet,resnet101_SpatialNet,resnet152_TemporalNet,resnet50_TemporalNet,resnet101_TemporalNet
+from VideoClassification.model.densenet_twostream.densenet_twostream import dense201_spatialNet,dense201_temporalNet,dense169_temporalNet,dense169_spatialNet
+from VideoClassification.model.C3D.C3D import C3D
 from VideoClassification.utils.Others.Logger import Logger
 from VideoClassification.utils.DataSetLoader.UCF101_DataSetLoader_FromFileName.UCF101Loader import train_UCF0101_Spatial,test_UCF0101_Spatial
 from VideoClassification.utils.Others.toolkits import accuracy,try_to_load_state_dict
@@ -18,30 +20,33 @@ from VideoClassification.utils.DataSetLoader.UCF101_DataSetLoader_FromFileName.P
 
 def Resenet152_SpatialNet_TestRuntime_Run():
 
-    model = resnet152_SpatialNet(pretrained=True,dropout=0.95).cuda()
+    model = dense201_spatialNet().cuda()
 
-    batchsize=32
+    for batchsize in [4,8,16,24,32]:
 
-    t1 = time.time()
+        total_gen = 0
+        total_batch = 0
 
-    pq_train = PictureQueue(dsl=train_UCF0101_Spatial(),Gen=GenVariables_Spatial,batchsize=batchsize)
 
-    t2 = time.time()
+        for i in range(15):
 
-    print('prepare_pq:',t2-t1)
+            t2 = time.time()
 
-    for i in range(30):
+            # imgs,labels = pq_train.Get()
 
-        t2 = time.time()
+            imgs = torch.randn(batchsize,3,224,224)
+            imgs = Variable(imgs).cuda(0)
 
-        imgs,labels = pq_train.Get()
+            t3 = time.time()
 
-        t3 = time.time()
+            pred =  model(imgs)
 
-        pred =  model(imgs)
+            t4 = time.time()
 
-        t4 = time.time()
+            total_gen += t3-t2
+            total_batch += t4-t3
 
-        print('gen_data:',t3-t2)
-        print('one batch',t4-t3)
+        print('batchsize:',batchsize)
+        print('gen_data:',total_gen/30)
+        print('one batch',total_batch/30)
 
