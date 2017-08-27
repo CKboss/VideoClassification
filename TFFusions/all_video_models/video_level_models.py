33a3fd7245f84cac103cd46c4f4f6786cedc4ac2
CKboss <qhn999@2008.sina.com>
2017-08-27 18:55:58 +0800

    chain moe model

--- TFFusions/all_video_models/video_level_models.py
+++ TFFusions/all_video_models/video_level_models.py
@@ -1,8 +1,11 @@
 from TFFusions.all_video_models.moe_model import MoeModel
+from TFFusions.all_video_models.chain_moe_model import ChainMoeModel
 
 
 def GetVideoModel(video_model_name):
     if video_model_name == 'moe_model':
         return MoeModel
+    elif video_model_name == 'chain_moe_model':
+        return ChainMoeModel
     else:
         raise NotImplementedError
