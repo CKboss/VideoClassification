83a9a8d4d8b7749320575523e468acefe5517d23
CKboss <qhn999@2008.sina.com>
2017-08-30 13:41:42 +0800

    ....backup....

--- TFFusions/train_scripts/train.py
+++ TFFusions/train_scripts/train.py
@@ -17,6 +17,7 @@ from TFFusions.tfrecord_tools import read_and_decode
 
 FLAGS = None
 
+
 def find_class_by_name(name, models):
     classes = [getattr(model, name, None) for model in models]
     if len(classes) == 1:
@@ -24,9 +25,10 @@ def find_class_by_name(name, models):
     else:
         return classes
 
+
 #############################################################################
 
-def split_into_small_peice(features,target_label,video_frames,fix_lenght=10,scale=8,one_hot=True):
+def split_into_small_peice(features, target_label, video_frames, fix_lenght=10, scale=8, one_hot=True):
     '''
     :param features:  a tensor batchsize x max_frams_len x features
     :param target_label: a tensor batchsize x 500
@@ -39,12 +41,10 @@ def split_into_small_peice(features,target_label,video_frames,fix_lenght=10,scal
         videoframes: (scale x batchsize)
     '''
     global FLAGS
-    try:
-        fix_lenght = FLAGS.fix_length
-        scale = FLAGS.scale
-    except:
-        fix_lenght = 10
-        scale = 8
+
+    fix_lenght = getattr(FLAGS, 'fix_length', fix_lenght)
+    scale = getattr(FLAGS, 'scale', scale)
+    one_hot = getattr(FLAGS, 'one_hot', one_hot)
 
     n = features.shape[0]
     n2 = target_label.shape[-1]
@@ -55,44 +55,39 @@ def split_into_small_peice(features,target_label,video_frames,fix_lenght=10,scal
     video_frames_ret = []
 
     for i in range(n):
-
         video_len = video_frames[i]
-        rid = random.choices(list(range(video_len)),k=scale)
-
-        # print('video_len: ',video_len)
+        rid = random.choices(list(range(video_len)), k=scale)
         if video_len <= fix_lenght:
-            video_len = fix_lenght+1
-
+            video_len = fix_lenght + 1
         for rg in rid:
-
             l = rg
-            r = rg+fix_lenght-1
-
-            if r >= video_len :
+            r = rg + fix_lenght - 1
+            if r >= video_len:
                 l = video_len - fix_lenght - 1
                 r = video_len - 2
-
-            features_ret.append( features[i,l:r+1,:] )
+            features_ret.append(features[i, l:r + 1, :])
             video_frames_ret.append(fix_lenght)
+            if one_hot == True:
+                target_label_ret.append(target_label[i])
+            else:
+                target_label_ret.append(np.argmax(target_label[i]))
 
-            if one_hot==True: target_label_ret.append(target_label[i])
-            else: target_label_ret.append(np.argmax(target_label[i]))
-
-    features_ret = np.vstack(features_ret).reshape(-1,fix_lenght,m)
+    features_ret = np.vstack(features_ret).reshape(-1, fix_lenght, m)
     video_frames_ret = np.array(video_frames_ret)
 
-    if one_hot==True: target_label_ret = np.vstack(target_label_ret).reshape(-1,n2)
-    else: target_label_ret = np.array(target_label_ret)
+    if one_hot == True:
+        target_label_ret = np.vstack(target_label_ret).reshape(-1, n2)
+    else:
+        target_label_ret = np.array(target_label_ret)
 
-    return features_ret,target_label_ret,video_frames_ret
+    return features_ret, target_label_ret, video_frames_ret
+
 
 #############################################################################
 
 
 def main(config_yaml=None):
-
     global FLAGS
-
     train_config = config_yaml or Config.TRAIN_SCRIPT + 'lstm-memory-cell1024.yaml'
     LOAD_YAML_TO_FLAG(train_config)
     FLAGS = Get_GlobalFLAG()
@@ -100,33 +95,37 @@ def main(config_yaml=None):
     if os.path.exists(FLAGS.train_dir) == False:
         print('mk train dir {}'.format(FLAGS.train_dir))
         os.mkdir(FLAGS.train_dir)
-
-    # train_items = getTrainItems()
-    # val_items = getValItems()
     batchsize = FLAGS.batchsize
 
-    #
+    # model
     if FLAGS.device_id != None:
-        os.environ['CUDA_VISIBLE_DEVICES']=str(FLAGS.device_id)[1:-1]
+        os.environ['CUDA_VISIBLE_DEVICES'] = str(FLAGS.device_id)[1:-1]
 
-    inputs = tf.placeholder(dtype=tf.float32, shape=(batchsize*FLAGS.scale, FLAGS.fix_length, 4096))
-    num_frames = tf.placeholder(dtype=tf.int32, shape=(batchsize*FLAGS.scale))
-    target_labels = tf.placeholder(dtype=tf.int32, shape=(batchsize*FLAGS.scale, FLAGS.vocab_size))
+    inputs = tf.placeholder(dtype=tf.float32, shape=(batchsize * FLAGS.scale, FLAGS.fix_length, 4096))
+    num_frames = tf.placeholder(dtype=tf.int32, shape=(batchsize * FLAGS.scale))
+    target_labels = tf.placeholder(dtype=tf.int32, shape=(batchsize * FLAGS.scale, FLAGS.vocab_size))
 
     model = GetFrameModel(FLAGS.frame_level_model)()
-    lossfunc = SoftmaxLoss()
 
-    predict_labels = model.create_model(model_input=inputs, vocab_size=FLAGS.vocab_size, num_frames=num_frames, num_mixtures=FLAGS.moe_num_mixtures)
+    predict_labels = model.create_model(model_input=inputs, vocab_size=FLAGS.vocab_size, num_frames=num_frames,
+                                        num_mixtures=FLAGS.moe_num_mixtures)
     predict_labels = predict_labels['predictions']
-    loss = lossfunc.calculate_loss(predict_labels, target_labels)
 
+    # loss
+    one_hot = getattr(FLAGS, 'one_hot', False)
+    if one_hot == False:
+        lossfunc = SoftmaxLoss()
+        loss = lossfunc.calculate_loss(predict_labels, target_labels)
+    else:
+        loss = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=predict_labels, labels=target_labels)
+
+    # optimize
     global_step = tf.Variable(0, trainable=False)
     decayed_learning_rate = tf.train.exponential_decay(FLAGS.base_learning_rate,
                                                        global_step,
                                                        FLAGS.decay_at_epoch,
                                                        FLAGS.learning_rate_decay,
                                                        staircase=True)
-
     optimizer_class = find_class_by_name(FLAGS.optimize, [tf.train])
     train_op = optimizer_class(decayed_learning_rate).minimize(loss)
 
@@ -180,7 +179,7 @@ def main(config_yaml=None):
 
     cnt = 0
 
-    for epoch in range(FLAGS.num_epochs+1):
+    for epoch in range(FLAGS.num_epochs + 1):
 
         # loop = len(train_items) // batchsize
         loop = 2222
@@ -188,31 +187,27 @@ def main(config_yaml=None):
 
         for i in range(loop):
 
-            features, target_label, video_frames, train_name = sess.run( [train_feature_batch, train_label_batch, train_frame_len_batch, train_name_batch])
-            # F = features
-            # T = target_label
-            # V = video_frames
-            # print(features.shape)
-            features, target_label, video_frames = split_into_small_peice(features,target_label,video_frames)
-            # print(features.shape)
-
-            fd = {inputs: features, target_labels: target_label, num_frames: video_frames}
-
+            input_features, input_target_labels, input_video_frames, train_name = sess.run(
+                [train_feature_batch, train_label_batch, train_frame_len_batch, train_name_batch])
+            input_features, input_target_labels, input_video_frames = split_into_small_peice(input_features,
+                                                                                             input_target_labels,
+                                                                                             input_video_frames)
+            fd = {inputs: input_features, target_labels: input_target_labels, num_frames: input_video_frames}
             loss_value, _ = sess.run([loss, train_op], feed_dict=fd)
-
             logger.scalar_summary(log_prefix_name + '/train_loss', loss_value, cnt)
             pylog.info('cnt: {} train_loss: {}'.format(cnt, loss_value))
 
             if cnt % 30 == 0:
-                features, target_label, video_frames, train_name = sess.run(
+                input_features, input_target_labels, input_video_frames, train_name = sess.run(
                     [train_feature_batch, train_label_batch, train_frame_len_batch, train_name_batch])
-
-                features, target_label, video_frames = split_into_small_peice(features,target_label,video_frames)
-                fd = {inputs: features, target_labels: target_label, num_frames: video_frames}
+                input_features, input_target_labels, input_video_frames = split_into_small_peice(input_features,
+                                                                                                 input_target_labels,
+                                                                                                 input_video_frames)
+                fd = {inputs: input_features, target_labels: input_target_labels, num_frames: input_video_frames}
 
                 predict = sess.run(predict_labels, feed_dict=fd)
-                train_meanap = mean_ap(predict, target_label)
-                acc = accuracy(predict, target_label, topk=(1, 5, 10))
+                train_meanap = mean_ap(predict, input_target_labels)
+                acc = accuracy(predict, input_target_labels, topk=(1, 5, 10))
 
                 logger.scalar_summary(log_prefix_name + '/train_mAP', train_meanap, cnt)
                 logger.scalar_summary(log_prefix_name + '/train_acc@1', acc[0], cnt)
@@ -224,19 +219,15 @@ def main(config_yaml=None):
                 pylog.info('cnt: {} train_acc@5: {}'.format(cnt, acc[1]))
                 pylog.info('cnt: {} train_acc@10: {}'.format(cnt, acc[2]))
 
-                # items = random.choices(val_items,k=FLAGS.batchsize)
-                # features, video_frames, target_label = gen_tf_input(items,'val')
-                # features, video_frames, target_label = pq_test.Get()
-
-                features, target_label, video_frames, test_name = sess.run(
+                input_features, input_target_labels, input_video_frames, test_name = sess.run(
                     [test_feature_batch, test_label_batch, test_frame_len_batch, test_name_batch])
-
-                features, target_label, video_frames = split_into_small_peice(features,target_label,video_frames)
-
-                fd = {inputs: features, target_labels: target_label, num_frames: video_frames}
+                input_features, input_target_labels, input_video_frames = split_into_small_peice(input_features,
+                                                                                                 input_target_labels,
+                                                                                                 input_video_frames)
+                fd = {inputs: input_features, target_labels: input_target_labels, num_frames: input_video_frames}
                 predict, test_loss = sess.run([predict_labels, loss], feed_dict=fd)
-                test_meanap = mean_ap(predict, target_label)
-                acc = accuracy(predict, target_label, topk=(1, 5, 10))
+                test_meanap = mean_ap(predict, input_target_labels)
+                acc = accuracy(predict, input_target_labels, topk=(1, 5, 10))
 
                 logger.scalar_summary(log_prefix_name + '/test_mAP', test_meanap, cnt)
                 logger.scalar_summary(log_prefix_name + '/test_acc@1', acc[0], cnt)
