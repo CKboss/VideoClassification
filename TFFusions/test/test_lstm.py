b2030976458860cca34a168cae576aa54d328a42
CKboss <qhn999@2008.sina.com>
2017-09-02 17:34:01 +0800

    ...

--- TFFusions/test/test_lstm.py
+++ TFFusions/test/test_lstm.py
@@ -25,7 +25,7 @@ def find_class_by_name(name, models):
     else:
         return classes
 
-train_config ='/datacenter/1/LSVC/Code/VideoClassification/TFFusions/Train/train_config_yaml/lstm-memory-cell1024.yaml'
+train_config ='/datacenter/1/LSVC/Code/VideoClassification/TFFusions/Train/train_config_yaml/lstm-memory-cell1024_testuse.yaml'
 # train_config ='/mnt/md0/LSVC/Code/VideoClassification/TFFusions/Train/train_config_yaml/lstm-memory-cell1024_testuse.yaml'
 LOAD_YAML_TO_FLAG(train_config)
 FLAGS = Get_GlobalFLAG()
@@ -63,7 +63,7 @@ decayed_learning_rate = tf.train.exponential_decay(float(FLAGS.base_learning_rat
 # learning_rate = tf.placeholder(dtype=tf.float32,shape=(1))
 
 optimizer_class = find_class_by_name(FLAGS.optimize, [tf.train])
-train_op = optimizer_class(decayed_learning_rate).minimize(loss)
+train_op = optimizer_class(decayed_learning_rate).minimize(loss,global_step=global_step)
 
 # init_op = tf.group(tf.global_variables_initializer(),tf.local_variables_initializer())
 init_op = tf.global_variables_initializer()
